2cc152683cdae697a9fbfa1a07664fb31feeaa86
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-06-16 13:59:35 +0100

    Controlling global scope completely

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -1,49 +1,108 @@
-var chatModule = (function(){
-	var _leadself = 'Me: ',
-	_leadcomputer = "PC: ",
-	_aSaid = ["This is a Cyber Chat"],
-	_msgYes = "Yes, that's a great idea.",
-	_msgNo = "No, that must be a mistake.",
-	_aSassyStuff = ["Like mold on books, grow myths on history.", "She moved like a poem and smiled like a sphinx.", 
-	"As long as we don’t die, this is gonna be one hell of a story.", 
-	"She laughed, and the desert sang.", "You’ve got about as much charm as a dead slug."];
-
-	function _echo(msg){
-		_aSaid.push("<div>" + msg + "</div>");
-		var aSaidLength = _aSaid.length, start = Math.max(aSaidLength - 6,0), out	= "";
-		for(var i = start; i < aSaidLength; i++){
-			out += _aSaid[i];
+// Controlling global access completely
+(function(win, doc, $){
+	var chatModule = (function(){
+		var _leadself = 'Me: ',
+		_leadcomputer = "PC: ",
+		_aSaid = ["This is a Cyber Chat"],
+		_msgYes = "Yes, that's a great idea.",
+		_msgNo = "No, that must be a mistake.",
+		_aSassyStuff = ["Like mold on books, grow myths on history.", "She moved like a poem and smiled like a sphinx.", 
+		"As long as we don’t die, this is gonna be one hell of a story.", 
+		"She laughed, and the desert sang.", "You’ve got about as much charm as a dead slug."];
+	
+		function _echo(msg){
+			_aSaid.push("<div>" + msg + "</div>");
+			var aSaidLength = _aSaid.length, start = Math.max(aSaidLength - 6,0), out	= "";
+			for(var i = start; i < aSaidLength; i++){
+				out += _aSaid[i];
+			}
+			$('.advert').html(out);
+			$('#talk span').text(msg);
+		} 
+	
+		function talk (msg){
+			_echo(_leadself + msg);
 		}
-		$('.advert').html(out);
-		$('#talk span').text(msg);
-	} 
+	
+		function replyYesNo(){
+			var msg = Math.random() > 0.5 ? _msgYes : _msgNo;
+			_echo(_leadcomputer + msg);
+		}
+	
+		function saySassyStuff(){
+			var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length)];
+			_echo( _leadcomputer + msg );
+		} 
+	
+		return {
+			talk:talk, 
+			replyYesNo:replyYesNo, 
+			saySassyStuff:saySassyStuff
+		};
+	})();
+	
+	$(doc).ready(function(){
+		chatModule.talk("This is great!");
+		chatModule.replyYesNo();
+		chatModule.saySassyStuff();
+	})
 
-	function talk (msg){
-		_echo(_leadself + msg);
-	}
+	if(!win.chatModule) win.chatModule = chatModule;
 
-	function replyYesNo(){
-		var msg = Math.random() > 0.5 ? _msgYes : _msgNo;
-		_echo(_leadcomputer + msg);
-	}
+})(window, document, jQuery);
 
-	function saySassyStuff(){
-		var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length)];
-		_echo( _leadcomputer + msg );
-	} 
 
-	return {
-		talk:talk, 
-		replyYesNo:replyYesNo, 
-		saySassyStuff:saySassyStuff
-	};
-})();
+console.log(window.chatModule);
 
-$(document).ready(function(){
-	chatModule.talk("This is great!");
-	chatModule.replyYesNo();
-	chatModule.saySassyStuff();
-})
+
+//Revealing Module Pattern
+
+// var chatModule = (function(){
+// 	var _leadself = 'Me: ',
+// 	_leadcomputer = "PC: ",
+// 	_aSaid = ["This is a Cyber Chat"],
+// 	_msgYes = "Yes, that's a great idea.",
+// 	_msgNo = "No, that must be a mistake.",
+// 	_aSassyStuff = ["Like mold on books, grow myths on history.", "She moved like a poem and smiled like a sphinx.", 
+// 	"As long as we don’t die, this is gonna be one hell of a story.", 
+// 	"She laughed, and the desert sang.", "You’ve got about as much charm as a dead slug."];
+
+// 	function _echo(msg){
+// 		_aSaid.push("<div>" + msg + "</div>");
+// 		var aSaidLength = _aSaid.length, start = Math.max(aSaidLength - 6,0), out	= "";
+// 		for(var i = start; i < aSaidLength; i++){
+// 			out += _aSaid[i];
+// 		}
+// 		$('.advert').html(out);
+// 		$('#talk span').text(msg);
+// 	} 
+
+// 	function talk (msg){
+// 		_echo(_leadself + msg);
+// 	}
+
+// 	function replyYesNo(){
+// 		var msg = Math.random() > 0.5 ? _msgYes : _msgNo;
+// 		_echo(_leadcomputer + msg);
+// 	}
+
+// 	function saySassyStuff(){
+// 		var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length)];
+// 		_echo( _leadcomputer + msg );
+// 	} 
+
+// 	return {
+// 		talk:talk, 
+// 		replyYesNo:replyYesNo, 
+// 		saySassyStuff:saySassyStuff
+// 	};
+// })();
+
+// $(document).ready(function(){
+// 	chatModule.talk("This is great!");
+// 	chatModule.replyYesNo();
+// 	chatModule.saySassyStuff();
+// })
 
 
 
