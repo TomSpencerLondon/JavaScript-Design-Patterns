552d90fbaa32838990172891c389750cd5e30e07
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-06-16 13:31:28 +0100

    Add javascript module pattern

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -1,32 +1,63 @@
-var o = {
-	leadself: 'Me: ',
-	leadcomputer: "PC: ",
-	aSaid: ["This is a Cyber Chat"],
-	msgYes: "Yes, that's a great idea.",
-	msgNo: "No, that must be a mistake.",
-	aSassyStuff: ["Like mold on books, grow myths on history.", "She moved like a poem and smiled like a sphinx.", 
+var chatModule = (function(){
+	var leadself = 'Me: ',
+	leadcomputer = "PC: ",
+	aSaid = ["This is a Cyber Chat"],
+	msgYes = "Yes, that's a great idea.",
+	msgNo = "No, that must be a mistake.",
+	aSassyStuff = ["Like mold on books, grow myths on history.", "She moved like a poem and smiled like a sphinx.", 
 	"As long as we don’t die, this is gonna be one hell of a story.", 
-	"She laughed, and the desert sang.", "You’ve got about as much charm as a dead slug."],
-	talk: function (msg){
-		this.echo(this.leadself + msg);
-	}, 
-	replyYesNo: function(){
-		var msg = Math.random() > 0.5 ? this.msgYes : this.msgNo;
-		this.echo( this.leadcomputer + msg);
-	}, 
-	saySassyStuff: function(){
-		var msg = this.aSassyStuff[Math.floor(Math.random() * this.aSassyStuff.length)];
-		this.echo( this.leadcomputer + msg );
-	}, 
-	echo: function(msg){
-	this.aSaid.push("<div>" + msg + "</div>");
-	var aSaidLength = this.aSaid.length, start = Math.max(aSaidLength - 6,0), out	= "";
-	for(var i = start; i < aSaidLength; i++){
-		out += this.aSaid[i];
+	"She laughed, and the desert sang.", "You’ve got about as much charm as a dead slug."];
+
+	function echo(msg){
+		aSaid.push("<div>" + msg + "</div>");
+		var aSaidLength = aSaid.length, start = Math.max(aSaidLength - 6,0), out	= "";
+		for(var i = start; i < aSaidLength; i++){
+			out += aSaid[i];
+		}
+		$('.advert').html(out);
+		$('#talk span').text(msg);
 	}
-	$('.advert').html(out);
-	$('#talk span').text(msg);
-	}
-}
+
+	return {
+		talk: function (msg){
+			echo(leadself + msg);
+		}, 
+		replyYesNo: function(){
+			var msg = Math.random() > 0.5 ? msgYes : msgNo;
+			echo(leadcomputer + msg);
+		}, 
+		saySassyStuff: function(){
+			var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
+			echo( leadcomputer + msg );
+		} 
+	};
+
+})();
+
+
+// var com = com || {};
+// com.tomspencer = com.tomspencer || {};
+// com.tomspencer.udemy = com.tomspencer.udemy || {};
+// com.tomspencer.udemy.JSDP = com.tomspencer.udemy.JSDP || {};
+
+// com.tomspencer.udemy.JSDP.sudoChat = {
+// 	leadself: 'Me: ',
+// 	leadcomputer: "PC: ",
+// 	aSaid: ["This is a Cyber Chat"],
+// 	msgYes: "Yes, that's a great idea.",
+// 	msgNo: "No, that must be a mistake.",
+// 	aSassyStuff: ["Like mold on books, grow myths on history.", "She moved like a poem and smiled like a sphinx.", 
+// 	"As long as we don’t die, this is gonna be one hell of a story.", 
+// 	"She laughed, and the desert sang.", "You’ve got about as much charm as a dead slug."], 
+// 	echo: function(msg){
+// 	this.aSaid.push("<div>" + msg + "</div>");
+// 	var aSaidLength = this.aSaid.length, start = Math.max(aSaidLength - 6,0), out	= "";
+// 	for(var i = start; i < aSaidLength; i++){
+// 		out += this.aSaid[i];
+// 	}
+// 	$('.advert').html(out);
+// 	$('#talk span').text(msg);
+// 	}
+// }
 
 
